f814806bc9fa58c2ab4c09a64f47a30eaa9d722c
brunoval <bhsousa@gmail.com>
2023-11-07 12:18:40 -0300

    simple controller

---
+++ src/main/java/com/brunosousa/pagamento/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.brunosousa.pagamento.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @GetMapping("/")
+    public String getUser() {
+        return "{\"user\": \"object\"}";
+    }
+}
